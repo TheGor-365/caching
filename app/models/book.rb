04416c333e07335502055ff5e121e9fe77009675
Gor <7495500@gmail.com>
2023-09-04 12:39:03 +0300

    Models caching

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :store, touch: true
+
+  def cached_books
+    Rails.cache.fetch("#{cache_key_with_version}/cached_books", expires_in: 2.seconds) do
+      Book.limit(100)
+    end
+  end
 end
