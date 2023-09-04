3258b604f4cc6f34cc05c92cea4791f6f5770775
Gor <7495500@gmail.com>
2023-09-04 12:55:19 +0300

    Caching configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,12 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :memory_store { size: 64.megabytes }
+    # config.cache_store = MyClassStore.new
+    # config.cache_store = :file_store, Rails.root.join("file_directory")
+    # config.cache_store = :mem_cach_store
+    # config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+    # config.cache_store = :null_store
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
@@ -38,9 +43,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
